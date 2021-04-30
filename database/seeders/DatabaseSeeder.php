4cc52ec12f7a23b597fb6709ce126b7ae4e6d2e4
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-29 19:19:01 -0700

    database seeding complete in Laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,18 +1,22 @@
 <?php
 
 namespace Database\Seeders;
-
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+    public function run() {
+        $faker = Faker::create();
+
+    	foreach (range(1,10) as $index) {
+            DB::table('employees')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'phone_number' => $faker->phoneNumber,
+                'dob' => $faker->date($format = 'D-m-y', $max = '2000',$min = '1990')
+            ]);
+        }
     }
 }
