3eb752cfcc056d41c8b1c1c888fb085cf323c713
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-29 19:53:53 -0700

    employee controller  edit for view page in Laravel

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -22,6 +22,6 @@ class EmployeeController extends Controller
       $pdf = PDF::loadView('pdf_view', $data);
 
       // download PDF file with download method
-      return $pdf->download('pdf_file.pdf');
+      return $pdf->download('pdf_view.pdf');
     }
 }
